2a3e5057a4fd29d44ddcca458ac6ca277e20a173
haebarassi <lovelymari99@naver.com>
2022-07-12 01:49:53 +0900

    CBV 적용

--- DjangoStudy-master/myBlog/blog/urls.py
+++ DjangoStudy-master/myBlog/blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views   # 현재 폴더의 views.py를 사용할 수 있게 가져온다.
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view())
 ]
--- DjangoStudy-master/myBlog/blog/views.py
+++ DjangoStudy-master/myBlog/blog/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+#    template_name = 'blog/post_list.html'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# FBV 방식
+#
+# def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+class PostDetail(DetailView):
+    model = Post
+
+# FBV 방식
+#
+# def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
+
+#    return render(
+#        request,
+#        'blog/single_post_page.html',
+#        {
+#            'post': post,
+#        }
+#    )
